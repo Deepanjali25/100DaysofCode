034ff3134738c27ef13ce8b7e92175efd0126157
Deepanjali25 <anjali.deep25@gmail.com>
2020-12-25 21:12:11 +0530

    DataStructures&Algorithms

--- src/QueueUsingStacks.java
+++ src/QueueUsingStacks.java
@@ -1,32 +1,24 @@
 // Java program to implement Queue using two stacks with costly enQueue()
-import java.util.*;
+import java.util.Stack;
 class QueueUsingStacks {
     static class Queue {
         static Stack<Integer> s1 = new Stack<Integer>();
         static Stack<Integer> s2 = new Stack<Integer>();
         static void enQueue(int x) {
-            // Move all elements from s1 to s2
             while (!s1.isEmpty()) {
-                s2.push(s1.pop());
-                //s1.pop();
+                s2.push(s1.pop()); // Move all elements from s1 to s2
             }
-            // Push item into s1
-            s1.push(x);
-            // Push everything back to s1
+            s1.push(x); // Push item into s1
             while (!s2.isEmpty()) {
-                s1.push(s2.pop());
-                //s2.pop();
+                s1.push(s2.pop()); // Push everything back to s1
             }
         }
-        // Dequeue an item from the queue
         static int deQueue() {
-            // if first stack is empty
-            if (s1.isEmpty()) {
+            if (s1.isEmpty()) { // if first stack is empty
                 System.out.println("Q is Empty");
                 System.exit(0);
             }
-            // Return top of s1
-            int x = s1.peek();
+            int x = s1.peek(); // Return top of s1
             s1.pop();
             return x;
         }
