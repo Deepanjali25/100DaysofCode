e4259df4225533df01edd6f043c7bc50bc248e7c
Deepanjali25 <anjali.deep25@gmail.com>
2020-12-29 15:14:36 +0530

    DataStructures&Algorithms

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,28 +1,27 @@
 public class Factorial {
-    public static void main(String[] args) {
-        System.out.println("Factorial of 5 using loop is:" + factorialWithLoop(5));
-        System.out.println("Factorial of 10 using recursion is:" + factorialWithRecursion(10));
-        System.out.println("Factorial of negative number -100 is:" + factorialWithLoop(-100));
-    }
-    public static long factorialWithLoop(int n) {
-        if(n < 0) {
-            System.out.println("Negative nos can't have factorial");
-            return -9999;
-        }
-        long fact = 1;
-        for (int i = 2; i <= n; i++) {
+    static int factorialWhileLoop(int n) {
+        if(n == 0)
+            return 1;
+        int i = n, fact = 1;
+        while (n / i != n) {
             fact = fact * i;
+            i--;
         }
         return fact;
     }
-    public static long factorialWithRecursion(int n) {
-        if(n < 0) {
-            System.out.println("Negative nos can't have factorial");
-            return -9999;
-        }
-        if (n <= 2) {
-            return n;
-        }
-        return n * factorialWithRecursion(n - 1);
+    static int factorialForLoop(int n) {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+    static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        return n * factorial(n - 1);
+    }
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is " + factorial(5));
     }
 }
